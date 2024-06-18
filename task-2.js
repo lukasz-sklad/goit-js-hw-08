a7a1761a047486cd3c327f96e0e3c98a1414f671
lukasz-sklad <lukasz.sklad@gmail.com>
2024-06-18 18:24:05 +0200

    Zadania

--- task-2.js
+++ task-2.js
@@ -1,2 +1,17 @@
 'use strict'
+    // Zadeklarowana funkcja formatMessage(message, maxLength)
+function formatMessage(message, maxLength) {
+    // Spr, czy długość wiadomości jest mniejsza lub równa maxLength
+    if (message.length <= maxLength) {
+        return message; // Zwróć oryginalną wiadomość
+    } else {
+        return message.slice(0, maxLength) + '...'; // Skróć wiadomość i dodaj "..."
+    }
+}
 
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
